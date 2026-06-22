a9f0eeeb7dfd020593d8840e79c4e86f088ac2ae
Pedro de carvalho gomes <pedrodecarvalhogomes@MacBook-Air-de-Pedro-2.local>
2026-06-22 18:35:07 -0300

    Primeira atepa comcluida com mepeamento e extracao das informacoes por anuncios

--- olx_eletronicos_pipeline/baixar.py
+++ olx_eletronicos_pipeline/baixar.py
@@ -2,13 +2,16 @@ import gzip
 from curl_cffi import requests
 from bs4 import BeautifulSoup as bs
 import os 
+import pandas  as pd 
+
+#source olx_eletronicos_pipeline/.venv/bin/activate
 
 
-sessao = requests.Session(impersonate="chrome")
-pasta = 'xml'
 
+def baixar_arquivo():
 
-def baixar_arquivo(sessao, pasta):
+    sessao = requests.Session(impersonate="chrome")
+    pasta = 'xml'
 
     links = ['https://www.olx.com.br/celulares/apple/estado-pe', 'https://www.olx.com.br/celulares/asus/estado-pe', 'https://www.olx.com.br/celulares/huawei/estado-pe', 'https://www.olx.com.br/celulares/infinix/estado_pe', 'https://www.olx.com.br/celulares/lenovo/estado-pe', 'https://www.olx.com.br/celulares/lg/estado-pe', 'https://www.olx.com.br/celulares/motorola/estado-pe', 'https://www.olx.com.br/celulares/samsung/estado-pe',  'https://www.olx.com.br/celulares/xiaomi/estado-pe']
     tam = len(links)
@@ -42,11 +45,16 @@ def lista_anuncios():
     lista_arquivo = os.listdir(caminho_xml)
     tam_lista = len(lista_arquivo)
     
+    
 
     for i in range(0, tam_lista):
         print("extraindo a lista de anuncios do arquivo:", lista_arquivo[i])
 
+        
+
         nome_arq = lista_arquivo[i]
+        if nome_arq.startswith('.'):
+            continue
         arquivo = os.path.join(caminho_xml, nome_arq)
 
         with open(arquivo) as f:
@@ -58,19 +66,67 @@ def lista_anuncios():
         for tag in lista_url:
 
             link = tag['href']
-
-            lista_link.append(link)
+            if link.startswith('https://pe.olx.com.br/') or link.startswith('https://www.olx.com.br/'):
+                            # Evita pegar links de páginas institucionais ou repetidos
+                            if 'detalhe' in link or '-anuncio-' in link or 'eletronicos-e-celulares' in link: 
+                                if link not in lista_link:
+                                    lista_link.append(link)
+    tam_url = len(lista_link)
     
 
-    return lista_link
+    return lista_link, tam_url
 
+def extrair(lista_link, tam_url):
+    sessao = requests.Session(impersonate="chrome")
+    
+    
+    dados_anuncios = []
 
-
+    for i in range(0, tam_url):
+        print(f"Coletando anúncio {i+1}/{tam_url}...")
         
-baixar_arquivo(sessao, pasta)
+        try:
+            requisicao = sessao.get(lista_link[i])
+            if requisicao.status_code != 200:
+                continue
+                
+            site = bs(requisicao.text, "html.parser")
+            spans = site.find_all("span")
+            
+           
+            if len(spans) < 65:
+                continue
 
-lista_anuncios()
+            
+            anuncio = {
+                "preço": spans[63].text.strip() if len(spans) > 63 else None,
+                "marca": spans[46].text.strip() if len(spans) > 46 else None,
+                "modelo": spans[48].text.strip() if len(spans) > 48 else None,
+                "armazenamento": spans[52].text.strip() if len(spans) > 52 else None,
+                "condição": spans[50].text.strip() if len(spans) > 50 else None,
+                "cor": spans[54].text.strip() if len(spans) > 54 else None,
+                "bateria": spans[56].text.strip() if len(spans) > 56 else None,
+                "localização": spans[59].text.strip() if len(spans) > 59 else None,
+                "discrição": spans[42].text.strip() if len(spans) > 42 else None
+            }
+            
+            dados_anuncios.append(anuncio)
+            
+        except Exception as e:
+            print(f"Erro ao acessar o link {lista_link[i]}: {e}")
+            continue
 
+    
+    if dados_anuncios:
+        df = pd.DataFrame(dados_anuncios)
+       
+        df.to_parquet('resultado_anuncios.parquet', index=False)
+        print("Arquivo Parquet salvo com sucesso!")
+    else:
+        print("Nenhum dado válido foi extraído.")
 
 
     
+baixar_arquivo()
+lista_link, tam_url = lista_anuncios()
+extrair(lista_link, tam_url)
